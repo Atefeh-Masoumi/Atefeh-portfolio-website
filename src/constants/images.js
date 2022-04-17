2209c05fa898a1bc16483f39a0e6926f5a98c3a9
Atefeh-Masoumi <atefeh.masoomy@gmail.com>
2022-04-18 00:28:09 +0430

    nav and header completed

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,22 +1,22 @@
 import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
 import api from '../assets/api.png';
-import cpp from '../assets/cpp.png';
+
 import css from '../assets/css.png';
 import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
+import reactheader from '../assets/reactheader.png';
 import git from '../assets/git.png';
-import graphql from '../assets/graphql.png';
+
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
 import mu5 from '../assets/mu5.png';
 import node from '../assets/node.png';
-import python from '../assets/python.png';
+
 import react from '../assets/react.png';
 import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
-import vue from '../assets/vue.png';
+
 
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
@@ -39,22 +39,18 @@ export default {
   email,
   mobile,
   api,
-  cpp,
   css,
   figma,
-  flutter,
+  reactheader,
   git,
-  graphql,
   html,
   javascript,
   mu5,
-  node,
-  python,
+  node,  
   react,
   redux,
   sass,
   typescript,
-  vue,
   about01,
   about02,
   about03,
