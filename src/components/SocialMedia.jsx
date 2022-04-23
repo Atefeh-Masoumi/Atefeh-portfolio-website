a24080b59c474140ccde924ff13872260b589c35
Atefeh-Masoumi <atefeh.masoomy@gmail.com>
2022-04-23 14:55:59 +0430

    work section completed

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import {FaTelegram} from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
+
     <div>
-      <BsGithub />
+      <a href='https://www.linkedin.com/in/atefeh-masoumi-b80a821ab/' target='_blank' rel='noreferrer'><BsLinkedin /></a>
     </div>
     <div>
-      <FaFacebookF />
+      <a href='https://github.com/Atefeh-Masoumi' target="_blank" rel="noreferrer"><BsGithub /></a>
     </div>
     <div>
-      
-      <a href='https://www.linkedin.com/in/atefeh-masoumi-b80a821ab/'><BsLinkedin /></a>
+     <a href='https://telegram.me/Atefeh652'><FaTelegram /></a> 
     </div>
+    
   </div>
 );
 
---
+++ src/container/Work/data.js
@@ -0,0 +1,60 @@
+import { images } from "../../constants"
+export const works =[
+   
+    {
+        _type: "works",
+    codeLink: "",
+    description: "SPA which I implemented the UI by using React JS",
+    imgUrl: images.about01,
+    projectLink: "",
+    tags: ["React JS", "All"],
+    title: "MERN E-commerce Website",
+    },
+    {
+        _type: "works",
+    codeLink: "",
+    description: "Live expense tracker to manage your budget using React JS",
+    imgUrl: images.about01,
+    projectLink: "",
+    tags: ["React JS","Web App", "All"],
+    title: "Expense Tracker",
+    },
+    {
+        _type: "works",
+    codeLink: "",
+    description: " My portfolio UI created using React JS, Sass",
+    imgUrl: images.about01,
+    projectLink: "",
+    tags: ["UI/UX", "All"],
+    title: "Portfolio Website ",
+    },
+    {
+        _type: "works",
+    codeLink: "",
+    description: "This Restaurant website is implemented with javascript,bootstrap, ajax,Json ",
+    imgUrl: images.about01,
+    projectLink: "",
+    tags: ["JavaScript", "All"],
+    title: "Chinese Bistro Website ",
+    },
+    {
+        _type: "works",
+    codeLink: "",
+    description: " A good Todo list is always a must implemented using React JS",
+    imgUrl: images.about01,
+    projectLink: "",
+    tags: ["React JS","Web App", "All"],
+    title: "Todo Application",
+    },
+    {
+        _type: "works",
+    codeLink: "",
+    description: "A good qoute is always inspiring",
+    imgUrl: images.about01,
+    projectLink: "",
+    tags: ["JavaScript", "All"],
+    title: "Qoute generator ",
+    },
+    
+    
+];
